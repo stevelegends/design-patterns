bd0a5512d2df191e624833971971393f2dd080d5
Steve <118787618+stevelegends@users.noreply.github.com>
2024-11-13 13:11:13 +0700

    Update factory.ts

--- factory.ts
+++ factory.ts
@@ -1,43 +1,53 @@
-
 interface IPayload {
   amount: number;
   address: string;
   signature: string;
 }
 
+interface IChainFactory<Payload> {
+  sendToken: (payload: Payload) => void;
+}
+
+// ETH Factory
 interface IETHPayload extends IPayload {
   publicKey: string;
   estimatedFee: number;
 }
-
-interface IBitcoinPayload extends IPayload {
-  fee: number;
-}
-
-interface IChainFactory {
-  sendETHToken: (payload: IETHPayload) => void;
-  sendBitcoinToken: (payload: IBitcoinPayload) => void;
-}
-
-// Factory
-const ChainFactory = (): IChainFactory => ({
-  sendETHToken: (payload: IPayload) => {},
-  sendBitcoinToken: (payload: IPayload) => {},
+interface IETHFactory extends IChainFactory<IETHPayload> {}
+const ETHFactory = (): IETHFactory => ({
+  sendToken(payload) {
+    console.log('Send ETH with payload ', payload);
+  },
 });
 
-interface IChain {
-  create: () => IChainFactory;
+// BTC Factory
+interface IBTCPayload extends IPayload {
+  fee: number;
 }
+interface IBTCFactory extends IChainFactory<IBTCPayload> {}
+const BTCFactory = (): IBTCFactory => ({
+  sendToken(payload) {
+    console.log('Send ETH with payload ', payload);
+  },
+});
+
 // Concrete Factory
-const Chain: IChain = {
-  create: () => {
-    return ChainFactory();
+interface IFactory {
+  create: <Factory>(
+    factory: () => IChainFactory<Factory>,
+  ) => IChainFactory<Factory>;
+}
+const ChainFactory: IFactory = {
+  create: Factory => {
+    return Factory();
   },
 };
 
-const chain = Chain.create();
+// Create Factory
+const eth = ChainFactory.create(ETHFactory);
+const btc = ChainFactory.create(BTCFactory);
 
-chain.sendETHToken({
+eth.sendToken({
   publicKey: '---public-key---',
   signature: 'RSA encrypted',
   address: '0xA...',
@@ -45,9 +55,28 @@ chain.sendETHToken({
   estimatedFee: 0.001,
 });
 
-chain.sendBitcoinToken({
+btc.sendToken({
   signature: 'RSA encrypted',
   address: '1BaX...',
   amount: 0.01,
   fee: 0.005,
+});
+
+// For example: extend a new Chain
+interface IBSCPayload extends IPayload {
+  gasFee: number;
+}
+interface IBSCFactory extends IChainFactory<IBSCPayload> {}
+const BSCFactory = (): IBSCFactory => ({
+  sendToken(payload) {
+    console.log('Send BSC with payload ', payload);
+  },
+});
+
+const bsc = ChainFactory.create(BSCFactory);
+bsc.sendToken({
+  amount: 10,
+  signature: '---signarure---',
+  gasFee: 0.001,
+  address: '0x8a8a...',
 });
