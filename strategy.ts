1f8aca93021aeaf0aaa67acfbea24a20739e942f
Steve <118787618+stevelegends@users.noreply.github.com>
2024-11-13 14:14:51 +0700

    Update strategy.ts

--- strategy.ts
+++ strategy.ts
@@ -1,27 +1,59 @@
-interface Chain {
-    sendTransaction: (address: string, amount: number) => void
+interface Notification {
+  title: string;
+  message: string;
 }
 
-const EHT: Chain = {
-    sendTransaction: (address, amount) => {
-        console.log('send eth tx');
-    }
+interface Notify {
+  sendMessage: (payload: Notification) => void;
 }
 
-const BTC: Chain = {
-    sendTransaction(address, amount) {
-        console.log('send btc tx');
+const SMS: Notify = {
+  sendMessage(payload) {
+    console.log('Sending SMS message', payload);
+  },
+};
+
+const Firebase: Notify = {
+  sendMessage(payload) {
+    console.log('Sending firebase message', payload);
+  },
+};
+
+// for example extend a new service
+const AWS: Notify = {
+  sendMessage(payload) {
+    console.log('Sending AWS message', payload);
+  },
+};
+
+const MessageStrategy = () => {
+  let context: Notify;
+  return {
+    setContext: (notify: Notify) => {
+      context = notify;
     },
+    executeSendMessage: (payload: Notification) => {
+      context.sendMessage(payload);
+    },
+  };
+};
+
+function runTime() {
+  const payload: Notification = {
+    title: 'Broadcast',
+    message: 'Message from system ...',
+  };
+
+  const message = MessageStrategy();
+  message.setContext(SMS);
+  message.executeSendMessage(payload);
+
+  message.setContext(Firebase);
+  message.executeSendMessage(payload);
+
+  // extend a new service
+  message.setContext(AWS);
+  message.executeSendMessage(payload);
 }
 
-const ChainStrategy = (chain: Chain) => {
-    return {
-        executeSendTransaction: chain.sendTransaction,
-    }
-}
-
-const eth = ChainStrategy(EHT);
-eth.executeSendTransaction('0x11..', 100);
-
-const btc = ChainStrategy(BTC);
-btc.executeSendTransaction('i8Aa..', 0.001);
+runTime();
